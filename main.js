f626841d884d4d27fe5bbcc7c2de6f8c1fa9c36a
Ikenna <ikennaugwuh@gmail.com>
2016-06-17 22:55:01 -0400

    Proof read the comments and added an alternative approach.

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+// minimize the native query selector funciton of the document
 var $ = function(sel){ return document.querySelector(sel); };
 
 // ================
 // QUESTION 1: Show me how to calculate the average price of all items.
 // ================
-// Declare a variable 'total'; for each item 'i' in 'items' add the items price to total
-var total = 0;
-items.forEach(function(i){ total += i.price; });
+// map through the items and collect the prices into an array; add the prices in the array and store the final amount in a variable called 'total'
+var total = items.map(function(i){ return i.price; }).reduce(function(a, b){ return a + b; });
+// ALTERNATE APPROACH: Declare a variable 'total'; for each item 'i' in 'items', add the items price to 'total'
+// var total = 0;
+// items.forEach(function(i){ total += i.price; });
 var average = total / items.length, // use 'total' and the number of items to find the average
-    ans1 = "The average price is $" + average.toFixed(2) + ".";
-$('#answer1').innerHTML = ans1;
+    ans = "The average price is $" + average.toFixed(2) + ".";
+$('#answer1').innerHTML = ans;
 
 
 // ================
@@ -17,7 +20,7 @@ $('#answer1').innerHTML = ans1;
 // Filter items that have a price between 14 and 18 and currency of USD into an array named 'filteredItems'
 var filteredItems = items.filter(function(i){ return i.price >= 14 && i.price <= 18 && i.currency_code == 'USD'; });
 filteredItems.forEach(function(i){ // for each filtered item 'i'
-    var itemName = document.createElement("p"); // create a node on the page with a tagname <p> and store it as a varialbe called 'itemName'
+    var itemName = document.createElement("p"); // create a node on the page with a tagname <p> and store it as a variable called 'itemName'
     itemName.textContent = i.title; // add the name of the filtered item inside the <p> tag 'itemName'
     $('#answer2').appendChild(itemName); // add the <p> tag 'itemName' to the page beneath Answer 2
 });
@@ -37,8 +40,8 @@ $('#answer3').innerHTML = priceInPounds[0].title; // add the title property of t
 // Filter items with a property 'materials' that contains a value called "wood" into an array called 'woodenItems'
 var woodenItems = items.filter(function(i){ return i.materials.includes('wood'); });
 woodenItems.forEach(function(i){ // for each wooden item 'i'
-    var itemName = document.createElement("p"); // create a node on the page with a tagname <p> and store it as a varialbe called 'itemName'
-    itemName.textContent = "\"" + i.title + '\" is made of wood.'; // add the name of the wooden item inside the <p> tag 'itemName'
+    var itemName = document.createElement("p"); // create a node on the page with a tagname <p> and store it as a variable called 'itemName'
+    itemName.textContent = "\"" + i.title + '\" is made of wood.'; // add the name of the wooden item 'i' inside the <p> tag 'itemName'
     $('#answer4').appendChild(itemName); // add the <p> tag 'itemName' to the page beneath Answer 2
 });
 
@@ -46,16 +49,16 @@ woodenItems.forEach(function(i){ // for each wooden item 'i'
 // ================
 // QUESTION 5: Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
 // ================
-// Filter items with a property 'materials' that contains 8 or more things into an array called 'fancyItems'
+// Filter items with a property 'materials' that contains 8 or more strings into an array called 'fancyItems'
 var fancyItems = items.filter(function(i){ return i.materials.length >= 8; });
 fancyItems.forEach(function(i){ // for each fancy item 'i'
-    var fancyItem = document.createElement("p"); // create a node on the page with a tagname <p> and store it as a varialbe called 'fancyItem'
-    var fancyItemMaterials = document.createElement("ol"); // create a node on the page (an ordered list) to list the materials of 'fancyItem'
+    var fancyItem = document.createElement("p"); // create a node on the page with a tagname <p> and store it as a variable called 'fancyItem'
+    var fancyItemMaterials = document.createElement("ol"); // create a node on the page (an ordered list <ol>) to list the materials of 'fancyItem'
     fancyItem.textContent = i.title; // add the name of the fancy item inside the <p> tag 'fancyItem'
-    i.materials.forEach(function(m){ // for each property 'materials' 'm' of the fancy item 'i'
-        var material = document.createElement("li"); // create a list item and store it as a variable called 'material'
+    i.materials.forEach(function(m){ // for each fancy item 'i' with a property 'materials' called 'm'
+        var material = document.createElement("li"); // create a list item <li> and store it as a variable called 'material'
         material.textContent = m; // add the material 'm' inside the list item
-        fancyItemMaterials.appendChild(material); // add the material to the ordered list 'fancyItemMaterials'
+        fancyItemMaterials.appendChild(material); // add the material <li> to the ordered list 'fancyItemMaterials'
     });
     fancyItem.appendChild(fancyItemMaterials); // add the ordered list 'fancyItemMaterials' to the <p> tag 'fancyItem'
     $('#answer5').appendChild(fancyItem); // add the <p> tag 'fancyItem' to the page beneath Answer 5
